fe632c22603b7029a73f365909ad75aaf4f1b72c
KevinZakky <kevinzakky9@gmail.com>
2024-07-04 23:26:27 +0700

    add cron schedule

--- routes/data_routes.js
+++ routes/data_routes.js
@@ -2,11 +2,11 @@ import express from "express";
 import {
   createDataKecamatan,
   getAllDataKecamatan,
-} from "../controllers/dataController";
+} from "../controllers/dataController.js";
 
 const router = express.Router();
 
-router.get("/data_kecamatan", getAllDataKecamatan);
-router.post("/data_kecamatan/:id", createDataKecamatan);
+router.get("/api/data_kecamatan", getAllDataKecamatan);
+router.post("/api/data_kecamatan", createDataKecamatan);
 
 export default router;
